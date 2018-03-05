eaa9bff1e6ba9b7070a932423f2227fd2661a5dc
Ujjwal Joshi <joshi_ujjwal@hotmail.com>
2018-03-05 19:00:47 +0545

    new test

---
+++ Proudct/Proudct/Controllers/ProductApi.cs
@@ -0,0 +1,41 @@
+﻿using Proudct.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Proudct.Controllers
+{
+    public class ProductApi : ApiController
+    {
+        private IMSEntities db = new IMSEntities();
+        // GET api/<controller>
+        public IEnumerable<Product> Get()
+        {
+            return db.Products.ToList();
+        }
+
+        // GET api/<controller>/5
+        public Product Get(int id)
+        {
+            return db.Products.Find(id);
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
